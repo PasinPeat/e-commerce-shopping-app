0bab049a24202fba16a6bd071c669de6b2455be2
PasinPeat <pasin.lawn1995@gmail.com>
2024-07-04 08:15:47 +0700

    fix:swiper integrated

--- components/homepage/HomepageProducts.tsx
+++ components/homepage/HomepageProducts.tsx
@@ -1,76 +1,20 @@
-"use client";
-import React, { useRef, useEffect, ReactNode, RefObject } from "react";
+// import React from "react";
 import ProductCard from "../productCard";
-import { Button, ButtonGroup } from "@nextui-org/button";
-import "swiper/css";
-import Swiper from "swiper";
-import { register } from "swiper/element/bundle";
-import { SwiperSlideProps } from "swiper/react";
-import { TestSwiper } from "../slider";
+import { Button } from "@nextui-org/button";
+import { ProductData } from "@/lib/productsample";
 
-register();
+import { TestSwipervertwo } from "../slidervertwo";
 
 type Props = {};
 
-const ProductData = [
-  {
-    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/T-SHIRT-WITH-TAPE-DETAILS.jpg",
-    name: "T-SHIRT WITH TAPE DETAILS",
-    price: 120,
-  },
-  {
-    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/SKINNY-FIT-JEANS.jpg",
-    name: "SKINNY FIT JEANS",
-    price: 240,
-  },
-  {
-    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/CHECKERED-SHIRT.jpg",
-    name: "CHECKERED SHIRT",
-    price: 180,
-  },
-  {
-    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/SLEEVE-STRIPED-T-SHIRT.jpg",
-    name: "SLEEVE STRIPED T-SHIRT",
-    price: 130,
-  },
-];
-
 export function NewArrivals({}: Props) {
-  // const swiperRef = useRef<SwiperRef>(null);
-
-  // useEffect(() => {
-  //   // listen for Swiper events using addEventListener
-  //   swiperRef.current.addEventListener("swiperprogress", (e: any) => {
-  //     const [swiper, progress] = e.detail;
-  //     console.log(progress);
-  //   });
-
-  //   swiperRef.current.addEventListener("swiperslidechange", (e: any) => {
-  //     console.log("slide changed");
-  //   });
-  // }, []);
-
   return (
-    <div className="my-4 flex flex-col gap-14">
+    <div className="my-4 flex flex-col  items-center gap-14">
       <h1 className="flex justify-center font-extrabold text-5xl my-8 ">
         New Arrival
       </h1>
-      <section className="">
-        {/* <swiper-container ref={swiperRef} slides-per-view="3">
-          <swiper-slide>
-            {ProductData.map((product, index) => (
-              <ProductCard
-                key={index}
-                src={product.src}
-                name={product.name}
-                price={product.price}
-              />
-            ))}
-          </swiper-slide>
-        </swiper-container> */}
-        
-        <TestSwiper/>
-      </section>
+      <TestSwipervertwo />
+
       <Button color="primary" className="w-[220px]">
         View All
       </Button>
---
+++ lib/productsample.ts
@@ -0,0 +1,28 @@
+type productdata = {
+  src: string;
+  name: string;
+  price: number;
+};
+
+export const ProductData: productdata[] = [
+  {
+    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/T-SHIRT-WITH-TAPE-DETAILS.jpg",
+    name: "T-SHIRT WITH TAPE DETAILS",
+    price: 120,
+  },
+  {
+    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/SKINNY-FIT-JEANS.jpg",
+    name: "SKINNY FIT JEANS",
+    price: 240,
+  },
+  {
+    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/CHECKERED-SHIRT.jpg",
+    name: "CHECKERED SHIRT",
+    price: 180,
+  },
+  {
+    src: "https://hmrwqjhhyatyjlnukuay.supabase.co/storage/v1/object/public/product/SLEEVE-STRIPED-T-SHIRT.jpg",
+    name: "SLEEVE STRIPED T-SHIRT",
+    price: 130,
+  },
+];
