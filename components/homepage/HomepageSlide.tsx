c733cd02e95889b5d55422a934e2e51eef9ac56c
PasinPeat <pasin.lawn1995@gmail.com>
2024-08-15 15:05:05 +0700

    fix:carousal bug

--- components/homepage/HomepageSlide.tsx
+++ components/homepage/HomepageSlide.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import { Pagination, Navigation } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import Image from "next/image";
 
 const imgSlide = [
@@ -27,7 +30,7 @@ function HomepageSlide() {
           pagination={{
             clickable: true,
           }}
-          modules={[Pagination, Navigation]}
+          modules={[Autoplay, Pagination, Navigation]}
           className="h-1/2 w-full"
         >
           {imgSlide.map((pic, index) => (
@@ -41,15 +44,6 @@ function HomepageSlide() {
               />
             </SwiperSlide>
           ))}
-          {/* <SwiperSlide>
-            <Image
-              className="w-full block"
-              width={700}
-              height={700}
-              alt="running"
-              src={imgSlide[0]}
-            />
-          </SwiperSlide> */}
         </Swiper>
       </div>
     </>
@@ -57,3 +51,41 @@ function HomepageSlide() {
 }
 
 export default HomepageSlide;
+
+// export const TestSwipervertwo = (data: any) => {
+//   const ProductData = [...data];
+
+//   return (
+//     <Swiper
+//       className="w-full h-full p-4"
+//       // spaceBetween={20}
+//       // slidesPerView={3}
+//       slidesPerView={"auto"}
+//       centeredSlides={true}
+//       loop={true}
+//       onSlideChange={() => console.log("slide change")}
+//       onSwiper={(swiper) => console.log(swiper)}
+//       autoplay={{
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       }}
+//       pagination={{
+//         clickable: true,
+//       }}
+//       // navigation={true}
+//       modules={[Autoplay, Pagination, Navigation]}
+//     >
+//       {imgSlide.map((pic, index) => (
+//         <SwiperSlide key={index}>
+//           <Image
+//             className="w-full block"
+//             width={700}
+//             height={700}
+//             alt="running"
+//             src={pic}
+//           />
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   );
+// };
