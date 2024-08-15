00ec1df1f65ee615a89228677a2e7015d8556904
PasinPeat <pasin.lawn1995@gmail.com>
2024-08-15 16:33:20 +0700

    fix:deploy bug

--- app/products/[slug]/(overview)/page.tsx
+++ app/products/[slug]/(overview)/page.tsx
@@ -8,13 +8,11 @@ import {
 import { CarouselSkeleton } from "@/app/ui/product/productSkeleton";
 import { CardsSkeletonHorizon } from "@/app/search/skeketon";
 import ProductDesc from "@/app/ui/product/productDesc";
-import ProductCarousel from "../../../ui/product/ProductCarousel";
+import ProductCarousel from "@/app/ui/product/ProductCarousel";
 import Productcolors from "@/app/ui/product/productcolors";
 import ProductStocks from "@/app/ui/product/productStocks";
 import OtherProducts from "@/app/ui/product/otherProducts";
 import Link from "next/link";
-
-
 
 async function ProductPage({ params }: { params: { slug: string } }) {
   const product = await fetchProductbySlug(params.slug);
